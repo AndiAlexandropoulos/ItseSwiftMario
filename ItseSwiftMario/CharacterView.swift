42385ef912a1fb7331a37e6a4bd131dcd5568e75
AndiAlexandropoulos <andiamarbeiten@gmail.com>
2022-12-16 13:36:57 +0100

    Initial Commit. Hi There

---
+++ ItseSwiftMario/CharacterView.swift
@@ -0,0 +1,49 @@
+//
+//  CharacterView.swift
+//  ItseSwiftMario
+//
+//  Created by andi a on 16.12.22.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    
+    var name : String
+    var imageName : String
+    var description : String
+    
+    var body: some View {
+        ZStack{
+            Color.cyan
+            HStack{
+                Image(imageName)
+                    .resizable()
+                    .frame(width: 160,height:160)
+                VStack{
+                    Text(name)
+                    Text(description)
+                    Button(action : sendMessage){
+                        Text("Message Me!")
+                            .tint(Color.blue)
+                    }
+                    .padding()
+                    .background(Color.white)
+                    .clipShape(Capsule())
+                }
+                
+            }
+        .padding()
+        }
+        .cornerRadius(24)
+        .padding()
+    }
+}
+
+struct CharacterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterView(name: "Wario", imageName: "Mario",
+        description:"Itse me, a italian Plummer looking for his Principessa")
+    }
+}
+
--- ItseSwiftMario/ContentView.swift
+++ ItseSwiftMario/ContentView.swift
@@ -8,19 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        ZStack{
+            ScrollView{
+                VStack{
+                    CharacterView(name: "Mario",
+                                  imageName: "Mario",
+                                  description: "desc 1")
+                    
+                    CharacterView(name: "Yoshi",
+                                  imageName: "Yoshi",
+                                  description: "desc 2")
+                    
+                    CharacterView(name: "GreenShell",
+                                  imageName: "GreenShell",
+                                  description: "desc 3")
+                    
+                    CharacterView(name: "Maria",
+                                  imageName: "Mario",
+                                  description: "desc 4")
+                }
+            }
         }
-        .padding()
-    }
+            
+            .cornerRadius(24)
+            .padding()
+            .opacity(0.85)
+            
+            HStack{
+                Image("Yoshi")
+                    .resizable()
+                    .frame(width: 120,height: 120)
+                Image("Mario")
+                    .resizable()
+                    .frame(width: 120,height: 120)
+                
+            }
+            VStack{
+                Image("GreenShell")
+                    .resizable()
+                    .frame(width: 120,height: 120)
+                Image("Mario")
+                    .resizable()
+                    .frame(width: 120,height: 120)
+            }
+            
+        }
+    
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func sendMessage(){
+        
     }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+
 }
