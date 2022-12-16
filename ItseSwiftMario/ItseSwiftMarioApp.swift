2e3a59fdcd98589d45d48c2278670b5fed4907ca
AndiAlexandropoulos <andiamarbeiten@gmail.com>
2022-12-16 11:14:42 +0100

    Initial Commit

---
+++ ItseSwiftMario/ItseSwiftMarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItseSwiftMarioApp.swift
+//  ItseSwiftMario
+//
+//  Created by andi a on 16.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ItseSwiftMarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
